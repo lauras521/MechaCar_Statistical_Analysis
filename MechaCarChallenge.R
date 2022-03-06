36f42b1a9549b22d14f1537944dd9521b6af17e7
lauras521 <laurastock521@gmail.com>
2022-03-05 21:26:19 -0600

    deliverable 1 summary statistics multiple linear regression

---
+++ MechaCarChallenge.R
@@ -0,0 +1,6 @@
+library(dplyr)
+mecha_car<-read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors=F)
+#generate multiple linear regression model
+lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_car)
+#generate summary statistics
+summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_car))
