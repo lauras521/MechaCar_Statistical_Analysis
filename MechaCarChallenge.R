13cf68f8e94b410646696e4cd3e48c625fff2c72
lauras521 <laurastock521@gmail.com>
2022-03-07 10:16:05 -0600

    deliverable 2 with summary statistics on suspension coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -4,3 +4,11 @@ mecha_car<-read.csv(file="MechaCar_mpg.csv",check.names=F,stringsAsFactors=F)
 lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_car)
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length+vehicle_weight+spoiler_angle+ground_clearance+AWD,data=mecha_car))
+
+
+suspension_coil<-read.csv(file="Suspension_Coil.csv",check.names=F,stringsAsFactors=F)
+total_summary <- summarize(suspension_coil, mean(PSI), median(PSI),var(PSI),sd(PSI))
+total_summary
+lot_groups<- group_by(suspension_coil, Manufacturing_Lot)
+lot_summary <- summarize(lot_groups, mean(PSI), median(PSI),var(PSI),sd(PSI))
+lot_summary
